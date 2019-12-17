67777896f02ebe841906b1e838757d7146351566
Barış Soner Uşaklı <barisusakli@gmail.com>
2019-12-17 15:24:31 -0500

    fix plugin hanging core (#3)
    * fix plugin hanging core
    
    * Update emailer-ses.js

--- emailer-ses.js
+++ emailer-ses.js
@@ -36,7 +36,8 @@ Emailer.init = function(params, callback){
 
 Emailer.send = function(data, callback){
     if (!ses) {
-        return winston.error('[emailer-ses] Connection to SES failed!');
+        winston.error('[emailer-ses] Connection to SES failed!');
+        return callback(null, data);
     } else {
         ses.sendEmail({
             Source: fromAddress,
@@ -58,10 +59,11 @@ Emailer.send = function(data, callback){
                     }
                 }
             }
-        }, function(err, data){
+        }, function(err, result){
             if (err) {
                 winston.error('[emailer-ses] Problem sending email: ' + err);
             }
+            callback(err, data);
         });
     }
 };
